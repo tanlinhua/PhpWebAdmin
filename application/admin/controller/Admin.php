a46bfa5bc5572b413904f2b158e41248a8abfff3
tanlinhua <493383500@qq.com>
2021-05-11 19:48:24 +0800

    换一种方式

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -35,15 +35,15 @@ class Admin extends Base
         $limits = trim(input('limit'));     //分页数量
         $search = trim(input("search"));    //检索
 
-        $whereSearch = " 1=1 ";
+        $db = db('admin')->alias('a')->join('role r', 'a.role=r.id', 'left')
+            ->where('a.role', 'NEQ', 0)->order('a.id asc')
+            ->field('a.id,role,user_name,role_name,created_at,updated_at,last_login_time,last_login_ip,status');
+
         if (!empty($search)) {
-            $whereSearch = " `user_name` LIKE '%$search%' ";
+            $db = $db->where('user_name', 'like', "%$search%");
         }
 
-        $result = db('admin')->alias('a')->join('role r', 'a.role=r.id', 'left')
-            ->where('a.role', 'NEQ', 0)->order('a.id asc')
-            ->field('a.id,role,user_name,role_name,created_at,updated_at,last_login_time,last_login_ip,status')
-            ->where($whereSearch)->paginate($limits, false, ['page'  => $page]); // 如果用model,一句sql搞不定,怎么破?
+        $result = $db->paginate($limits, false, ['page'  => $page]);
 
         return success('success', $result->total(), $result->items());
     }
