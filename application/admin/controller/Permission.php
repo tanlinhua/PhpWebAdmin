9064550754b72806cbfab3e95c7c5d4e3352dc69
tanlinhua <493383500@qq.com>
2021-05-12 18:46:06 +0800

    获取菜单树

--- application/admin/controller/Permission.php
+++ application/admin/controller/Permission.php
@@ -31,9 +31,46 @@ class Permission extends Base
         return success('success', $result->total(), $result->items());
     }
 
-    // 获取树形菜单数据
-    public function tree()
+    // 获取树形菜单数据1
+    public function tree($roleid = 0)
     {
-        return error();
+        $tree = $this->getTreeData($roleid);
+        return success('success', 0, $tree);
+    }
+
+    // 获取树形菜单数据2
+    public function getTreeData($roleid = 0)
+    {
+        $ids = db('role')->where('id', $roleid)->value('per_id');
+        $ids = explode(',', $ids);
+
+        $root = db('permission')->select();
+        $lowLevel = db('permission')->max('level'); //最小子菜单级别表示
+
+        $tree = $this->getTreeMenu($root, $ids, $lowLevel);
+        return $tree;
+    }
+
+    // 将权限表数据递归分级
+    protected function getTreeMenu($data, $ids = array(), $lowLevel, $pid = 0, $deep = 0)
+    {
+        $tree = array();
+        foreach ($data as $row) {
+            // 针对layui进行处理 👇
+            $row['title'] = $row['name'];
+            $row['checked'] = false;
+            $row['spread'] = true;
+            if ($row['level'] == $lowLevel) {
+                if (in_array($row['id'] . '', $ids)) {
+                    $row['checked'] = true; //有权限则选择
+                }
+            }
+            // 处理子菜单
+            if ($row['pid'] == $pid) {
+                $row['children'] = $this->getTreeMenu($data, $ids, $lowLevel, $row['id'], $deep + 1);
+                $tree[] = $row;
+            }
+        }
+        return $tree;
     }
 }
