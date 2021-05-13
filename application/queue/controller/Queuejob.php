ee393260d96fb8eeae392e27dd515326dea24bcc
tanlinhua <493383500@qq.com>
2021-05-13 16:41:15 +0800

    add think-queue demo

---
+++ application/queue/controller/Queuejob.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\queue\controller;
+
+use think\Queue\Job;
+
+class Queuejob
+{
+    /**
+     * fire方法是消息队列默认调用的方法
+     * @param Job $job 当前的任务对象
+     * @param $data 发布任务时自定义的数据
+     * @return int
+     */
+    public function fire(Job $job, $data)
+    {
+        //这里$data定义格式为：$data = [ 'type'=>1, 'data_id' => 123,'ts' => time()]
+        if (empty($data)) {
+            return 0;
+        }
+
+        if (is_array($data) && isset($data['type'])) {
+            $type = $data['type'];
+            if ($type == 1) {
+                //执行发送邮件业务
+                $isJobDone = $this->sendEmail($data['data_id']);
+            } else if ($type == 2) {
+                //执行APP推送消息业务
+                $isJobDone = $this->sendAppMessage($data['data_id']);
+            } else if ($type == 3) {
+                //执行订单业务
+                $isJobDone = $this->orderService($data['data_id']);
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+        if ($isJobDone) {
+            // 如果任务执行成功，删除任务
+            $job->delete();
+        } else {
+            if ($job->attempts() > 3) {
+                //通过这个方法可以检查这个任务已经重试了几次了
+                $job->delete();
+                // 也可以重新发布这个任务
+                //$job->release(2); //$delay为延迟时间，表示该任务延迟2秒后再执行
+            }
+        }
+    }
+
+    public function failed($data)
+    {
+        // ...任务达到最大重试次数后，失败了
+    }
+
+    //发邮件业务
+    private function sendEmail($id)
+    {
+        trace('sendEmail', 'error');
+        return true;
+    }
+
+    //发App消息业务
+    private function sendAppMessage($id)
+    {
+        trace('sendAppMessage', 'error');
+        return true;
+    }
+
+    //处理订单业务
+    private function orderService($id)
+    {
+        trace('orderService', 'error');
+        return true;
+    }
+}
---
+++ application/queue/controller/Queuetest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\queue\controller;
+
+use think\Controller;
+use think\Queue;
+
+class Queuetest extends Controller
+{
+    public function test()
+    {
+        return 'queue.test';
+    }
+
+    /**
+     * 发送队列消息
+     * nohup php think queue:listen --queue MyJobs &
+     */
+    public function index()
+    {
+        // 1.当前任务将由哪个类来负责处理。
+        //   当轮到该任务时，系统将生成一个该类的实例，并调用其 fire 方法
+        $jobHandlerClassName  = 'app\jobs\controller\Queuejob';
+
+        // 2.当前任务归属的队列名称，如果为新队列，会自动创建
+        $jobQueueName  = "MyJobs";
+
+        // 3.当前任务所需的业务数据 . 不能为 resource 类型，其他类型最终将转化为json形式的字符串
+        //   ( jobData 为对象时，存储其public属性的键值对 )
+        $jobData = ['type' => 1, 'data_id' => 12, 'ts' => time()];
+
+        // 4.将该任务推送到消息队列，等待对应的消费者去执行
+        $isPushed = Queue::push($jobHandlerClassName, $jobData, $jobQueueName);
+
+        if ($isPushed !== false) {
+            return '消息已发出';
+        } else {
+            return '消息发送出错';
+        }
+    }
+}
